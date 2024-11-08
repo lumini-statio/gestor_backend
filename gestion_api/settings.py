d63d266bb886535d3e42e9ca0a49157a2d8adf69
Emilio <lunaemilio2003@gmail.com>
2024-11-08 18:48:50 -0300

    nuevas migraciones

--- gestion_api/settings.py
+++ gestion_api/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i1ws!an4=g)=w+t3uykos*cw15=za%lsyuz0si7h5@o5i=8opb'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
